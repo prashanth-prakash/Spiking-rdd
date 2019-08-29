Dimensions corrected backprop is running

lib/lif.py
@@ -12,22 +12,27 @@ train_set, valid_set, test_set = cPickle.load(f)
 f.close()
 
 
-nb_classes = 10
-data = [[0,1,2,3,4,5,6,7,8,9]]
 
+# dataset returns numbers
+# Create a lookup table for the labels
 def indices_to_one_hot(data, nb_classes):
     """Convert an iterable of indices to one-hot encoded labels."""
     targets = np.array(data).reshape(-1)
     return np.eye(nb_classes)[targets]
+nb_classes = 10
+data = [[0,1,2,3,4,5,6,7,8,9]]
+one_hot_lookup=indices_to_one_hot(data,10) 
 
-one_hot_lookup=indices_to_one_hot(data,10)
+### Data has been loaded   
+
+
 
 def mnist_spike(lamba,D,kernel):
     
     rand_num=random.randint(0,train_set[0].shape[0])
     mnist_rand=(train_set[0][rand_num]) 
     label_rand=train_set[1][rand_num]
-    label_rand_onehot=one_hot_lookup[label_rand-1,:]
+    label_rand_onehot=one_hot_lookup[label_rand,:]
     new_var=np.zeros([784,D])
     for i in range(mnist_rand.shape[0]):
         x = np.random.uniform(0,1,D)
@@ -52,8 +57,8 @@ def activation(u,xw,std,gl,theta):
 def integrate(xw,std,gl,theta):
     return quad(activation,0,np.inf,args=(xw, std,gl,theta))
 
-def gradient(x,sigma,gl,theta):
-    return (np.diff(integrate(x,sigma,gl,theta)))
+def gradient(x):
+    return (np.diff(x))
 
 def convolve_online(s, h, kernel, t_offset):
     if len(s.shape) > 1:
@@ -670,8 +675,8 @@ class LIF_Recurrent(object):
         #Simulate t seconds
        
         for iter in range(self.params.iterations):
-            self.x,self.y=mnist_spike(lamba=0.02,D=self.T,kernel=self.params.kernel)
-            print("size of output:",self.y)
+            self.x,self.y = mnist_spike(lamba=0.02,D=self.T,kernel=self.params.kernel)
+            #print("size of output:",self.y)
             inp[iter,:,:]=self.x
           #  print(iter)
             
@@ -705,7 +710,6 @@ class LIF_Recurrent(object):
                 #Decrement the refractory counters
                 r[r>0] -= 1
             
-            self.vt = vt
             self.backprop(h,epoch=1)
             #self.sh = sh
             
@@ -716,39 +720,34 @@ class LIF_Recurrent(object):
         std=20
         gl=0.1
         theta=0.5
-        #print(self.W.shape)
+
         print(self.x.shape)
-        #print(self.U.shape)
         vec_integral=np.vectorize(integrate)
+        
         for ep in range(epoch):
-            #xw=np.dot(self.W,self.x)
-            hidden = np.mean(self.sh,1)# average over t timesteps 
+
+            hidden = np.mean(self.sh[0:self.params.n1,:],1)# average over t timesteps 
             print(hidden.shape)
-            y_hat=np.mean(self.sh[self.params.n1::],1) # 10 outs
-            #hidden=vec_integral(xw, self.params.sigma1,gl,theta)
-            #print(hidden)
-            #hidU=np.dot(hidden,self.U)
-            #y_hat=vec_integral(hidU, self.params.sigma2,gl,theta)
+            
+            y_hat=np.mean(self.sh[self.params.n1:,:],1) # 10 outs            
             e=y_hat-self.y
             e=e.reshape([y_hat.shape[0],1])
+            
             print("e shape:",e.shape)
-            print("sh shape:",np.mean(self.sh[self.params.n1::,:],1).shape)
-            dLdu=np.multiply(e,vec_integral(np.mean(self.sh[self.params.n1::,:],1),self.params.sigma1,gl,theta)[0])
-            print("dLdu shape:",dLdu.shape)
-            delta=np.multiply(e,vec_integral(self.U[self.params.n1:,0:self.params.n1],
-                                             self.params.sigma1,gl,theta)[0])#,gradient(self.sh))
-            print("delta shape:",delta.shape)
-            print("size W:",self.W.shape)
-            #print("size tmp:",vec_integral(h[0:self.params.n1,:],self.params.sigma1,gl,theta)[0].shape)
-            tmp2=np.dot(delta.T,vec_integral(np.mean(self.sh[self.params.n1:,:],1),self.params.sigma1,gl,theta)[0])
-            print(tmp2.shape)
-            tmp3=np.multiply(tmp2,np.mean(self.sh[0:self.params.n1,:],1))
-            tmp3=np.reshape(tmp3,[tmp3.shape[0],1])
-            #print(np.mean(self.x,1).shape)
-            dLdw=np.dot(tmp3,np.reshape(np.mean(self.x,1),[784,1]).T)
+            
+            padded1 = np.pad(gradient(vec_integral(y_hat,self.params.sigma1,gl,theta)[0]),(0,1),'constant', constant_values=(0))
+            padded1=np.reshape(padded1,[10,1])
+            delta = np.multiply(e,padded1)
+            hidden=np.reshape(hidden,[200,1])
+            dLdU = np.dot(delta,hidden.T)
+            
+            print("dLdu shape:",dLdU.shape)
+
+            tmp=np.dot(delta.T,self.U[self.params.n1:,0:self.params.n1])
+            dLdw=np.dot(tmp.T,np.reshape(np.mean(self.x,1),[784,1]).T)
             print("dLdw shape:",dLdw.shape)
             self.W=self.W-self.params.learning_rate*dLdw
-            self.U[self.params.n1:,0:self.params.n1]=self.U[self.params.n1:,0:self.params.n1]-self.params.learning_rate*dLdu
+            self.U[self.params.n1:,0:self.params.n1]=self.U[self.params.n1:,0:self.params.n1]-self.params.learning_rate*dLdU
             
         return 
      